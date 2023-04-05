c2dbb38c2209e86285f47c1cb7ef474eda1ccc10
Thanagorn Ruangsit <truangsi@z2t16c1.42bangkok.com>
2023-04-05 18:47:18 +0700

    added rectangle and blackground

--- fractol.c
+++ fractol.c
@@ -6,18 +6,18 @@
 /*   By: truangsi <truangsi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 15:22:58 by truangsi          #+#    #+#             */
-/*   Updated: 2023/04/03 15:50:39 by truangsi         ###   ########.fr       */
+/*   Updated: 2023/04/05 17:46:07 by truangsi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	handle_no_event(void *data)
-{
-	if (data)
-		return (0);
-	return (0);
-}
+// int	handle_no_event(void *data)
+// {
+// 	if (data)
+// 		return (0);
+// 	return (0);
+// }
 
 int	handle_input(int keysym, t_frac *fr)
 {
@@ -29,20 +29,17 @@ int	handle_input(int keysym, t_frac *fr)
 
 int	handle_keypress(int key, t_frac *fr)
 {
-	printf("in press\n");
 	if (key == K_ESC)
+	{
 		mlx_destroy_window(fr->mlx, fr->win_ptr);
-	printf("key: %d\n", key);
-	exit (0);
+		exit (0);
+	}
+	return (0);
 }
 
-int	handle_keyrelease(int key, void *data)
+int	encode_rgb(uint8_t red, uint8_t green, uint8_t blue)
 {
-	printf("in release\n");
-	printf("key: %d\n", key);
-	if (data)
-		return (0);
-	return (0);
+	return (red << 16 | green << 8 | blue);
 }
 
 void	my_mlx_pixel_put(t_frac *fr, int x, int y, int color)
@@ -51,6 +48,47 @@ void	my_mlx_pixel_put(t_frac *fr, int x, int y, int color)
 
 	dst = fr->addr + (y * fr->line_length + x * (fr->bits_per_pixel / 8));
 	*(unsigned int *) dst = color;
+}
+
+int	render_rect(t_frac *fr, t_rect rect, int color)
+{
+	int	i;
+	int j;
+
+	if (!fr->win_ptr)
+		return (1);
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+			while (j < rect.x + rect.width)
+				my_mlx_pixel_put(fr, j++, i, color);
+			++i;
+	}
+	return (0);
+}
+
+void	render_background(t_frac *fr, int color)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < WINDOW_HEIGHT)
+	{
+		j = 0;
+		while (j < WINDOW_WIDTH)
+			my_mlx_pixel_put(fr, j++, i, color);
+		i++;
+	}
+}
+
+void	render(t_frac *fr)
+{
+	render_background(fr, GREY);
+	render_rect(fr, (t_rect) {WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100}, RED);
+	render_rect(fr, (t_rect) {0, 0, 100, 100}, BLUE);
+	mlx_put_image_to_window(fr->mlx, fr->win_ptr, fr->img, 0, 0);
 }
 
 int	main(void)
@@ -67,17 +105,18 @@ int	main(void)
 	// create image
 	fr.img = mlx_new_image(fr.mlx, 1000, 1080);
 	fr.addr = mlx_get_data_addr(fr.img, &fr.bits_per_pixel, &fr.line_length, &fr.endian);
-	my_mlx_pixel_put(&fr, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(fr.mlx, fr.win_ptr, fr.img, 0, 0);
+	// my_mlx_pixel_put(&fr, 5, 5, 0x00FF0000);
+	// render_background(&fr, GREY);
+	// render_rect(&fr, (t_rect) {WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100}, RED);
+	// render_rect(&fr, (t_rect) {0, 0, 100, 100}, BLUE);
+	// mlx_put_image_to_window(fr.mlx, fr.win_ptr, fr.img, 0, 0);
 
 	// setup hooks
 	// mlx_loop_hook(fr.mlx, &handle_no_event, &fr);
-	// mlx_key_hook(fr.win_ptr, &handle_input, &fr);
-	mlx_loop_hook(fr.mlx, &handle_no_event, &fr);
+	mlx_key_hook(fr.win_ptr, &handle_input, &fr);
+	render(&fr);
+	// mlx_loop_hook(fr.mlx, &handle_no_event, &fr);
 	mlx_hook(fr.win_ptr, 2, 0, &handle_keypress, &fr);
-	// mlx_hook(fr.win_ptr, 3, 0, &handle_keyrelease, &fr);
 
 	mlx_loop(fr.mlx);
-	// exit the loop if no window left, and execute this code
-	mlx_destroy_window(fr.mlx, fr.win_ptr);
 }
--- fractol.h
+++ fractol.h
@@ -6,17 +6,32 @@
 /*   By: truangsi <truangsi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 15:42:16 by truangsi          #+#    #+#             */
-/*   Updated: 2023/04/03 15:50:56 by truangsi         ###   ########.fr       */
+/*   Updated: 2023/04/05 16:27:21 by truangsi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
+#define WINDOW_HEIGHT 1080
+#define WINDOW_WIDTH 1000
+#define RED 0x00FF0000
+#define BLUE 0x00002E95
+#define GREY 0x007E8C93
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <mlx.h>
 #include "key.h"
+
+typedef struct s_rect
+{
+	int		x;
+	int		y;
+	int		width;
+	int		height;
+	// int		color;
+} t_rect;
 
 typedef struct s_frac
 {
